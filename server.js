593a15ae653c01070ec6c46924069e168e1ec1d0
markocurlin <marko.curlin22@gmail.com>
2022-06-15 00:20:16 +0200

    Commit

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const dataRouter = require('./routes/data');
+//const dataRouter = require('./routes/data');
+
+DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.get('/', cors(corsOptions), (req, res) => {
     res.json({ "name": 'Marko1234' });
@@ -14,7 +24,21 @@ app.get('/marko', (req, res) => {
     res.json({ "name": 'Aaa' });
 });
 
-app.use('/data', dataRouter);
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM sensordata');
+      const results = { 'results': (result) ? result.rows : null};
+      //res.render('pages/db', results );
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.json("Error " + err);
+    }
+})
+
+//app.use('/data', dataRouter);
 
 var corsOptions = {
     origin: 'https://wi-se-client.vercel.app/',
