d00285b3422473c7c57a8cbecafed120a3d9a9b0
markocurlin <marko.curlin22@gmail.com>
2022-06-15 00:13:50 +0200

    Commit

--- routes/data.js
+++ routes/data.js
@@ -25,16 +25,17 @@ const pool = new Pool({
 
 DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
 
-router.get('/db', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM sensordata');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    //res.render('pages/db', results );
+    res.json(results);
     client.release();
   } catch (err) {
     console.error(err);
-    res.send("Error " + err);
+    res.json("Error " + err);
   }
 })
 
