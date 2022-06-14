d7218d386fbd3470cadb01897ffa6fd20efbc7f1
markocurlin <marko.curlin22@gmail.com>
2022-06-15 01:35:01 +0200

    Commit

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const dataRouter = require('./routes/data');
-/*
-DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});*/
 
 app.get('/', cors(corsOptions), (req, res) => {
     res.json({ "name": 'Marko1234' });
@@ -24,27 +14,17 @@ app.get('/marko', (req, res) => {
     res.json({ "name": 'Aaa' });
 });
 
-/*
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM sensordata');
-      const results = { 'results': (result) ? result.rows : null};
-      //res.render('pages/db', results );
-      res.json(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.json("Error " + err);
-    }
-})
-*/
-
 app.use('/data', dataRouter);
 
+/*
 var corsOptions = {
     origin: 'https://wi-se-client.vercel.app/',
     optionsSuccessStatus: 200
+}*/
+
+var corsOptions = {
+  origin: 'https://eod17j16agf5tgv.m.pipedream.net/',
+  optionsSuccessStatus: 200
 }
 
 /*
