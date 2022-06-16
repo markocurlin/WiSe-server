e288abb3685dbac89ffee853f63a4ed4b251280a
markocurlin <marko.curlin22@gmail.com>
2022-06-16 20:50:05 +0200

    Commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 //const dataRouter = require('./routes/data');
 const transformdata = require('./services/transformdata');
-
+/*
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -15,7 +15,7 @@ const pool = new Pool({
 });
 
 DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
-
+*/
 
 var corsOptions = {
   origin: 'https://wi-se-client.vercel.app/',
