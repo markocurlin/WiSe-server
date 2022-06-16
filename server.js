78e729a80591048e6732daa51000112562594485
markocurlin <marko.curlin22@gmail.com>
2022-06-16 20:47:49 +0200

    Commit

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ const pool = new Pool({
 
 DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
 
-/*
+
 var corsOptions = {
   origin: 'https://wi-se-client.vercel.app/',
   optionsSuccessStatus: 200
-}*/
-
+}
+/*
 app.use(cors({
   origin: 'https://wi-se-client.vercel.app/',
   credentials: true
-}));
+}));*/
 
 
 app.use(bodyParser.urlencoded({
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/', /*cors(corsOptions),*/ (req, res) => {
+app.get('/', cors(corsOptions), (req, res) => {
   var str = "";
   
 	for (var i = 0 ; i < globalMQTT.length; i++)
