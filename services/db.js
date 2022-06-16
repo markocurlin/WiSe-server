ae8f1db339b9b06da3240ac0405e170c9825ff4d
markocurlin <marko.curlin22@gmail.com>
2022-06-16 12:29:55 +0200

    Commit

--- services/db.js
+++ services/db.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2/promise');
-const config = require('../config');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-async function query(sql, params) {
-  const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
-
-  return results;
-}
+DATABASE_URL = 'postgres://vaqvtedfomzxdc:d77cc333c761c0314ff96440010f658e4c95e9066c58ffb2460c2d76764f4ed9@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dbga6btbrrmhl7';
 
 module.exports = {
-  query,
+  pool
 }
