84dda381a362a046f32344acc1f428ca94b9daae
markocurlin <marko.curlin22@gmail.com>
2022-06-17 20:02:19 +0200

    Commit

--- server.js
+++ server.js
@@ -47,46 +47,17 @@ app.all('*', function(req, res, next) {
 let globalData = '';
 /*cors(corsOptions),*/
 
-app.post('/',  async (req, res) => {
-  const temp = transformdata.transformHexToDec(globalMQTT);
-  globalData = transformdata.transformString(temp);
-
-  if (globalData.length !== 0) {
-    try {
-      const client = await pool.connect();
-      const result = await client.query(`INSERT INTO sensordata(temperature, humidityair, lux, humiditysoil)VALUES(${globalData[0]}, ${globalData[1]}, ${globalData[2]}, ${globalData[3]})`);
-      res.json(globalData);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.json("Error " + err);
-    }
-  }
-  //res.json(globalData);
-  //res.json('13 14 15 17');
-});
-
-/*
 app.get('/',  async (req, res) => {
   const temp = transformdata.transformHexToDec(globalMQTT);
   globalData = transformdata.transformString(temp);
 
   if (globalData.length !== 0) {
-    try {
-        const client = await pool.connect();
-        const result = await client.query(`INSERT INTO sensordata(temperature, humidityair, lux, humiditysoil)VALUES(${globalData[0]}, ${globalData[1]}, ${globalData[2]}, ${globalData[3]})`);
-        //const results = { 'results': (result) ? result.rows : null};
-        res.json(globalData);
-        client.release();
-      } catch (err) {
-        console.error(err);
-        res.json("Error " + err);
-      }
-    }
+    res.json(globalData);
+  }
   //res.json(globalData);
   //res.json('13 14 15 17');
 });
-*/
+
 //app.use('/data', dataRouter);
 //baza podataka
 
