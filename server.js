b5b0f2ac17cdcd4c0a6ea4f1c273ab7e8e045ed1
markocurlin <marko.curlin22@gmail.com>
2022-06-18 14:26:06 +0200

    Commit

--- server.js
+++ server.js
@@ -79,6 +79,21 @@ app.post('/data',  async (req, res) => {
   }
 })
 
+app.post('/', async (req, res) => {
+  if (globalData.length === 4) {
+    try {
+      client = await pool.connect();
+      const result = await client.query(`INSERT INTO sensordata(temperature, humidityair, lux, humiditysoil)VALUES(${globalData[0]}, ${globalData[1]}, ${globalData[2]}, ${globalData[3]})`);
+      //const results = { 'results': (result) ? result.rows : null};
+      res.json(result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.json("Error " + err);
+    }
+  }
+});
+/*
 app.post('/insertdata', async (req, res) => {
   if (globalData.length === 4) {
     try {
@@ -93,7 +108,7 @@ app.post('/insertdata', async (req, res) => {
     }
   }
 });
-
+*/
 //
 
 app.use((err, req, res, next) => {
